ee4d160771d6597cf2d58d00acbff6e3470e95e8
Ilya_Hancharyk <Ilya_Hancharyk@epam.com>
2026-05-25 14:59:05 +0200

    EPMRPP-114990 || Code review fixes - 1

--- ui/src/constants/common.ts
+++ ui/src/constants/common.ts
@@ -1,3 +1,4 @@
 export const SECRET_FIELDS_KEY = 'rp_secretFieldsToClear';
 
+/** Same value as `pluginId` in root `gradle.properties` */
 export const PLUGIN_NAME = 'mobitru';
--- ui/src/hooks/useCommandExecutor.ts
+++ ui/src/hooks/useCommandExecutor.ts
@@ -14,10 +14,8 @@
  * limitations under the License.
  */
 
+import { PLUGIN_NAME } from 'constants/common';
 import type { UtilsInterface } from 'extensionProps/utils';
-
-/** Same value as `pluginId` in root `gradle.properties` (first argument to `URLS.pluginsCommandsCommon`). */
-export const PLUGIN_NAME = 'mobitru';
 
 /**
  * Example for **organization / instance** UI: **`POST`** on **`URLS.pluginsCommandsCommon`**
