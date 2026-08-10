de6c64a420528c08431ba3dc3c0b18f1c5070f06
siarheirazuvalau <siarhei_razuvalau@epam.com>
2026-08-10 16:18:43 +0300

    EPMRPP-118043 || Add scrolling on maintenance page for small screens

--- ui/src/components/emptyStateMaintenance/emptyStateMaintenance.tsx
+++ ui/src/components/emptyStateMaintenance/emptyStateMaintenance.tsx
@@ -1,31 +1,16 @@
-/*
- * Copyright 2026 EPAM Systems
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { Button, RefreshIcon } from '@reportportal/ui-kit';
 import classNames from 'classnames/bind';
 import parse from 'html-react-parser';
 import RpLogo from 'icons/logo-white.svg';
 import SatelliteIllustration from 'icons/satellite.svg';
 import { messages } from 'messages/cloudDevices';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useIntl } from 'react-intl';
 
 import { PageBreadcrumb, PageBreadcrumbs } from '../pageBreadcrumbs';
 import { SocialLinks } from '../socialLinks';
 import styles from './emptyStateMaintenance.scss';
+const MAXIMAL_HEIGHT = 720;
 
 const cx = classNames.bind(styles);
 
@@ -41,6 +26,30 @@ const EmptyStateMaintenance = ({
   breadcrumbTree = [],
 }: EmptyStateMaintenanceProps): React.ReactElement => {
   const { formatMessage } = useIntl();
+
+  useEffect(() => {
+    const layoutContentElement = document.querySelector<HTMLElement>('[class*="layout__content"]');
+
+    if (!layoutContentElement) {
+      return () => {};
+    }
+
+    const handleResize = () => {
+      if (window.innerHeight <= MAXIMAL_HEIGHT) {
+        layoutContentElement.style.setProperty('overflow-y', 'auto', 'important');
+      } else {
+        layoutContentElement.style.removeProperty('overflow-y');
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      layoutContentElement.style.removeProperty('overflow-y');
+    };
+  }, []);
 
   const handleRefresh = useCallback(() => {
     onRefreshClick();
