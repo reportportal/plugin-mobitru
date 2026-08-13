79a3c683b3d22686d699a035f0410975853b4085
Alla Prischepa <38905429+allaprischepa@users.noreply.github.com>
2026-08-13 12:05:46 +0400

    EPMRPP-118610 || The scroll and last elements of the page are hidden due to demo banner (#44)
    * EPMRPP-118610 || The scroll and last elements of the page are hidden due to demo banner
    
    * EPMRPP-118610 || Keep demo banner fixed while scrolling the maintenance page

--- ui/src/components/emptyStateMaintenance/emptyStateMaintenance.tsx
+++ ui/src/components/emptyStateMaintenance/emptyStateMaintenance.tsx
@@ -20,13 +20,12 @@ import parse from 'html-react-parser';
 import RpLogo from 'icons/logo-white.svg';
 import SatelliteIllustration from 'icons/satellite.svg';
 import { messages } from 'messages/cloudDevices';
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import { useIntl } from 'react-intl';
 
 import { PageBreadcrumb, PageBreadcrumbs } from '../pageBreadcrumbs';
 import { SocialLinks } from '../socialLinks';
 import styles from './emptyStateMaintenance.scss';
-const MAXIMAL_HEIGHT = 720;
 
 const cx = classNames.bind(styles);
 
@@ -43,67 +42,45 @@ const EmptyStateMaintenance = ({
 }: EmptyStateMaintenanceProps): React.ReactElement => {
   const { formatMessage } = useIntl();
 
-  useEffect(() => {
-    const layoutContentElement = document.querySelector<HTMLElement>('[class*="layout__content"]');
-
-    if (!layoutContentElement) {
-      return () => {};
-    }
-
-    const handleResize = () => {
-      if (window.innerHeight <= MAXIMAL_HEIGHT) {
-        layoutContentElement.style.setProperty('overflow-y', 'auto', 'important');
-      } else {
-        layoutContentElement.style.removeProperty('overflow-y');
-      }
-    };
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      layoutContentElement.style.removeProperty('overflow-y');
-    };
-  }, []);
-
   const handleRefresh = useCallback(() => {
     onRefreshClick();
   }, [onRefreshClick]);
 
   return (
     <div className={cx('maintenance-page')}>
-      <PageBreadcrumbs breadcrumbs={breadcrumbs} breadcrumbTree={breadcrumbTree} />
+      <div className={cx('maintenance-canvas')}>
+        <PageBreadcrumbs breadcrumbs={breadcrumbs} breadcrumbTree={breadcrumbTree} />
 
-      <div className={cx('logo')}>{parse(RpLogo)}</div>
+        <div className={cx('logo')}>{parse(RpLogo)}</div>
 
-      <div className={cx('moon-wrapper')}>
-        <div className={cx('satellite')} aria-hidden="true">
-          {parse(SatelliteIllustration)}
-        </div>
-
-        <div className={cx('content')}>
-          <div className={cx('text-group')}>
-            <h2 className={cx('title')}>{formatMessage(messages.maintenanceTitle)}</h2>
-            <p className={cx('description')}>
-              {formatMessage(messages.maintenanceDescription)}
-              <br />
-              {formatMessage(messages.maintenanceDescription2)}
-            </p>
-
-            <Button
-              variant="primary"
-              onClick={handleRefresh}
-              icon={<RefreshIcon />}
-              iconPlace="start"
-              className={cx('refresh-btn')}
-            >
-              {formatMessage(messages.maintenanceButtonRefresh)}
-            </Button>
+        <div className={cx('moon-wrapper')}>
+          <div className={cx('satellite')} aria-hidden="true">
+            {parse(SatelliteIllustration)}
           </div>
 
-          <div className={cx('social-section')}>
-            <SocialLinks />
+          <div className={cx('content')}>
+            <div className={cx('text-group')}>
+              <h2 className={cx('title')}>{formatMessage(messages.maintenanceTitle)}</h2>
+              <p className={cx('description')}>
+                {formatMessage(messages.maintenanceDescription)}
+                <br />
+                {formatMessage(messages.maintenanceDescription2)}
+              </p>
+
+              <Button
+                variant="primary"
+                onClick={handleRefresh}
+                icon={<RefreshIcon />}
+                iconPlace="start"
+                className={cx('refresh-btn')}
+              >
+                {formatMessage(messages.maintenanceButtonRefresh)}
+              </Button>
+            </div>
+
+            <div className={cx('social-section')}>
+              <SocialLinks />
+            </div>
           </div>
         </div>
       </div>
